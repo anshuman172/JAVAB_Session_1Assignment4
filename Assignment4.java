522a72719cbdd391ef1d9e5c3b39fd6815a36ae7
anshuman172 <anshuman172@gmail.com>
2016-04-10 15:33:50 +0530

    Assignment4

---
+++ Assignment4.java
@@ -0,0 +1,16 @@
+package session1;
+
+public class Assignment4 {
+
+	public static void main(String[] args) {
+		
+		int x=5, y=10;
+		
+		System.out.println("x + y * 2 : " + x+y*2);
+		System.out.println("x - y * 2 : " + (x-y)*2);
+		System.out.println("(x + y) * 2 : " + (x+y)*2);
+		System.out.println("y%x : " + y%x);
+
+	}
+
+}
